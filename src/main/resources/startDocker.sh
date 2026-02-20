9c461072eafaa3acc8560d776110aa7eca64f2a7
Daniel Naro <dnaro@ac.upc.edu>
2026-02-20 09:46:04 +0100

    Avoiding port conflicts

--- src/main/resources/startDocker.sh
+++ src/main/resources/startDocker.sh
@@ -1,1 +1,1 @@
-docker run --name beacon-postgres -p 5432:5432 -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+docker run --name beacon-postgres -p 6432:6432 -e POSTGRES_PASSWORD=mysecretpassword -d postgres
